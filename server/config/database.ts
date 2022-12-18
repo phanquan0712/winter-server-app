82205bdd493fb905f0b90df72c68525582aee1b0
Winter <92377024+phanquan0712@users.noreply.github.com>
2022-12-18 15:40:34 +0700

    Update database.ts

--- server/config/database.ts
+++ server/config/database.ts
@@ -2,6 +2,8 @@ import mongoose from 'mongoose';
 
 const URI = process.env.MONGODB_URI;
 mongoose.connect(`${URI}`, (err: any) => {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
    if(err) throw err;
    console.log('Connected to MongoDB');
 })
